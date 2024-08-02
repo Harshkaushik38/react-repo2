f9d0b2bca9b605f29e20a33410b4898d078f39b1
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-08-02 13:54:08 +0530

    add new items

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const domelement = (
+    <a href="https://www.google.com"> visit google</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com',target: '_blank'},
+    'click me to visited google'
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    reactElement
 
 )
