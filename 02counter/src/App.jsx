73935989c99846f108c9a9950f3dcaf0bf5cf655
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-08-06 10:44:44 +0530

    add new feature

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,12 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter =15;
+
+  let [counter,setcounter] =useState(15);
+
+  // let counter =15;
 
   const addvalue = () => {
     console.log("value added", counter);
     counter = counter + 1;
+  setcounter(counter);
+ if (addvalue>15 && addvalue<1) {
+  console.log("do not print");
+ }
+  }
 
+  const removevalue =() => {
+    setcounter(counter -1);
   }
 
   return (
@@ -18,10 +28,11 @@ function App() {
       <h1>React with harsh</h1>
       <h1>Counter value {counter}</h1>
 
-      <button onClick={addvalue}>Add value</button>
+      <button onClick={addvalue}>Add value {counter}</button>
       
       <br />
-      <button>Remove value</button>
+      <button onClick={removevalue}
+      >Remove value {counter}</button>
         
     </>
   )
