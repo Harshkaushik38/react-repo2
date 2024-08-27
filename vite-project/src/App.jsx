573623035547f0237478ac64c08d1cb3aaee0ba9
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-08-27 13:40:42 +0530

    add new work

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,16 @@
-import Harry from "./chai"
- 
- function Chai() {
-
-    return(
-       <Harry/>
-    )
- }
 
 
- 
+function App(){
+   const username = "this is react code";
+   return(
+     <>
+      <h2>hello ! this is react project</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nisi quibusdam quidem. Officiis veritatis suscipit cumque incidunt magni odit repellat impedit fugit velit {username}.</p>
+      </>
+   )
+}
 
- export default Chai;
+export default App;
+
+
+
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
 const reactElement = React.createElement(
     'a',
-    {href: 'https:/www.google.com', target:'_blank'},
+    {href :'https:/www.google.com',target :'_blank'},
     'click here to visit google'
 
     
@@ -14,7 +14,6 @@ const reactElement = React.createElement(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    reactElement
-
+<App/>
 )
   
